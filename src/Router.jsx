3abf3ffe21ad6d4a8e7e8a543aa273b8cc203b16
kangminguu <mingudat9909@gmail.com>
2024-10-04 13:53:03 +0900

    page route

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 import NotFound from "./NotFound";
@@ -9,7 +9,7 @@ import SearchPage from "./features/SearchPage/SearchPage";
 import DetailPage from "./features/DetailPage/DetailPage";
 import WishListPage from "./features/WishListPage/WishListPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <App />,
@@ -26,20 +26,3 @@ const router = createBrowserRouter([
 export default function Router() {
     return <RouterProvider router={router} />;
 }
-
-// export default function PageRouter() {
-//     return (
-//         <Router>
-//             <Routes>
-//                 <Route path="/" element={<App />}>
-//                     <Route index element={<HomePage />} />
-//                     <Route path="search" element={<SearchPage />} />
-//                     <Route path="detail/:id" element={<DetailPage />} />
-//                     <Route path="wish" element={<WishListPage />} />
-//                 </Route>
-//                 <Route path="*" element={<NotFound />} />{" "}
-//                 {/* NotFound 페이지를 마지막에 추가 */}
-//             </Routes>
-//         </Router>
-//     );
-// }
