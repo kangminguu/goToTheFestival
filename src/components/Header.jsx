2760c2f90790794c7fde1109d82519593a472130
kangminguu <mingudat9909@gmail.com>
2024-10-04 13:59:55 +0900

    fix: 로고 경로 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import { LightModeIcon } from "./ui/icon/index.js";
-// import logo from "../assets/logo.svg"
+import logo from "../assets/logo.svg"
 
 export default function Header() {
     return (
         <div className="max-w-[480px] w-[100%] h-[52px] px-[16px] z-50 bg-[#FFFFFF] flex justify-between fixed top-0">
             <div className="h-[100%] flex flex-col justify-center">
-                <img className="h-[24px]" src="/logo.svg" alt="" />
+                <img className="h-[24px]" src={logo} alt="" />
             </div>
             <div className="h-[100%] flex flex-col justify-center">
                 <LightModeIcon />
