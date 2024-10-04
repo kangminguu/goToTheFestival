d83e1ca9d79526a85b6bb1dafde612ef126c6ea0
kangminguu <mingudat9909@gmail.com>
2024-10-04 11:54:26 +0900

    page

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/goToTheFestival/',
   css: {
     postcss: {
         plugins: [ tailwindcss() ],
-        base: '/goToTheFestival/'
     }
   }
 })
