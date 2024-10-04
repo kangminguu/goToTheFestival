0719c37422007f5f1b7b38bfaa932b55cf8bb173
kangminguu <mingudat9909@gmail.com>
2024-10-04 12:05:37 +0900

    fix: 로고 안보이는거 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="max-w-[480px] w-[100%] h-[52px] px-[16px] z-50 bg-[#FFFFFF] flex justify-between fixed top-0">
             <div className="h-[100%] flex flex-col justify-center">
-                <img className="h-[24px]" src="/logo.svg" alt="" />
+                <img className="h-[24px]" src="../assets/logo.svg" alt="" />
             </div>
             <div className="h-[100%] flex flex-col justify-center">
                 <LightModeIcon />
